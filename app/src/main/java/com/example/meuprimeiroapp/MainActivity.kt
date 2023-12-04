302c57a88233d0a3f6325ef908408465f550d85c
renan <renanmonego1@gmail.com>
2023-12-04 08:59:03 -0300

    commit inicial

---
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.meuprimeiroapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity() : AppCompatActivity(), Parcelable {
+
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btncalcular: Button = findViewById(R.id.btncalcular)
+        val edtPeso: EditText = findViewById(R.id.edittext_peso)
+        val edtAltura: EditText = findViewById(R.id.edittext_altura)
+
+
+
+        btncalcular.setOnClickListener {
+
+            val alturaStr = edtAltura.text.toString()
+            val pesoStr = edtPeso.text.toString()
+
+            if (alturaStr.isNotEmpty() && pesoStr.isNotEmpty()) {
+                val altura: Float = alturaStr.toFloat()
+                val peso: Float = pesoStr.toFloat()
+
+                val alturaFinal: Float = altura * altura
+                val result: Float = peso / alturaFinal
+
+                val intent = Intent(this, ResultActivity::class.java)
+                    .apply {
+                        putExtra("EXTRA_RESULT", result)
+                    }
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+    }
+
+}
